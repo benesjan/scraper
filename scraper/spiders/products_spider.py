2ef19e76d9b1645c43f998dae935c9f75cb6e977
Jan Beneš <janbenes1234@gmail.com>
2017-12-25 12:01:07 +0100

    Bug fix of not going through deeper subsections

--- scraper/spiders/products_spider.py
+++ scraper/spiders/products_spider.py
@@ -11,16 +11,23 @@ class ProductsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        # Select main sections from from start_urls
-        for url in response.xpath('//li/h2/a/@href'):
-            yield response.follow(url.extract(), callback=self.parse_section)
+        # Check if there is a main section
+        if response.xpath('//div/div/div/h2/text()').extract_first() == 'Hlavní sekce':
+            # Select main sections from from start_urls
+            for url in response.xpath('//li/h2/a/@href'):
+                yield response.follow(url.extract(), callback=self.parse)
+        else:
+            # parse_section method is a generator hence can't be called directly
+            for item in self.parse_section(response):
+                yield item
 
     def parse_section(self, response):
         # Process the page of products
         for product_url in response.xpath('//div/h2/a/@href'):
             url = product_url.extract()
             # Possibly will require more robust (regex based) validation
-            yield {'url': url, 'valid': (url.endswith('/') and url.count('/') == 4)}
+            if url.endswith('/'):
+                yield {'url': url}
 
         # Notify scrapy to process next page
         next_page = response.css('a.next::attr(href)').extract_first()
